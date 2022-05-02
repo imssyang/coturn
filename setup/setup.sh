a74f52d36bf6c0cc8cc050366b23a3418d2488ad
imssyang <imssyang@gmail.com>
2022-05-02 23:10:52 +0800

    update turnserver

--- setup/setup.sh
+++ setup/setup.sh
@@ -1,112 +1,110 @@
 #!/bin/bash
 
 APP=coturn
-GROUP=turnserver
-USER=turnserver
 HOME=/opt/$APP
 SYSD=/etc/systemd/system
-SERFILE=turnserver.service
 
-init() {
-  egrep "^$GROUP" /etc/group >/dev/null
-  if [[ $? -ne 0 ]]; then
-    groupadd -r $GROUP
+_mkdir() {
+  name=$1
+  if [[ ! -d $name ]]; then
+    mkdir -p $name
   fi
+}
 
-  egrep "^$USER" /etc/passwd >/dev/null
-  if [[ $? -ne 0 ]]; then
-    useradd -r -g $GROUP -s /usr/sbin/nologin -M $USER
+_rmdir() {
+  name=$1
+  if [[ -d $name ]]; then
+    rm -rf $name
   fi
+}
 
-  if [[ ! -d $HOME/var/db ]]; then
-    mkdir -p $HOME/var/db
-    chmod 755 $HOME/var/db
+_create_symlink() {
+  src=$1
+  dst=$2
+  if [[ ! -d $dst ]] && [[ ! -s $dst ]]; then
+    ln -s $src $dst
+    echo "($APP) create symlink: $src -> $dst"
   fi
+}
 
-  if [[ ! -d $HOME/var/run ]]; then
-    mkdir -p $HOME/var/run
-    chmod 755 $HOME/var/run
+_delete_symlink() {
+  dst=$1
+  if [[ -d $dst ]] || [[ -s $dst ]]; then
+    rm -rf $dst
+    echo "($APP) delete symlink: $dst"
   fi
+}
 
-  if [[ ! -d $HOME/var/log ]]; then
-    mkdir -p $HOME/var/log
-    chmod 755 $HOME/var/log
-  fi
-
-  chown -R $GROUP:$USER $HOME
-
-  if [[ ! -s $SYSD/$SERFILE ]]; then
-    ln -s $HOME/setup/$SERFILE $SYSD/$SERFILE
-    systemctl enable $SERFILE
-    echo "($APP) create symlink: $SYSD/$SERFILE --> $HOME/setup/$SERFILE"
-  fi
-
+_enable_service() {
+  name=$1
+  _create_symlink $HOME/setup/$name $SYSD/$name
+  systemctl enable $name
   systemctl daemon-reload
 }
 
-deinit() {
-  if [[ -s $SYSD/$SERFILE ]]; then
-    systemctl disable $SERFILE
-    rm -rf $SYSD/$SERFILE
-    echo "($APP) delete symlink: $SYSD/$SERFILE"
-  fi
+_disable_service() {
+  name=$1
+  systemctl disable $name
+  systemctl daemon-reload
+  _delete_symlink $SYSD/$name
+}
 
-  egrep "^$USER" /etc/passwd >/dev/null
-  if [[ $? -eq 0 ]]; then
-    userdel $USER
-  fi
+_start_service() {
+  name=$1
+  systemctl start $name
+  systemctl status $name
+}
 
-  egrep "^$GROUP" /etc/group >/dev/null
-  if [[ $? -eq 0 ]]; then
-    groupdel $GROUP
-  fi
+_stop_service() {
+  name=$1
+  systemctl stop $name
+  systemctl status $name
+}
+
+init() {
+  _mkdir $HOME/var/db
+  _mkdir $HOME/var/log
+  _mkdir $HOME/var/run
 
   chown -R root:root $HOME
+  chmod 755 $HOME
+
+  _enable_service turnserver.service
+}
+
+deinit() {
+  _rmdir $HOME/var/db
+  _rmdir $HOME/var/log
+  _rmdir $HOME/var/run
+
+  chown -R root:root $HOME
+  chmod 755 $HOME
+
+  _disable_service turnserver.service
 }
 
 start() {
-  pgrep -x turnserver >/dev/null
-  if [[ $? != 0 ]]; then
-    systemctl start $SERFILE
-    echo "($APP) turnserver start!"
-  fi
-  show
+  _start_service turnserver.service
 }
 
 stop() {
-  pgrep -x turnserver >/dev/null
-  if [[ $? == 0 ]]; then
-    systemctl stop $SERFILE
-	echo "($APP) turnserver stop!"
-  fi
-  show
+  _stop_service turnserver.service
 }
 
 show() {
-  ps -ef | grep turnserver | grep -v 'grep'
+  systemctl status turnserver.service
 }
 
 case "$1" in
-  init)
-    init
-    ;;
-  deinit)
-    deinit
-    ;;
-  start)
-    start
-    ;;
-  stop)
-    stop
-    ;;
-  show)
-	show
-	;;
-  *)
-    SCRIPTNAME="${0##*/}"
-    echo "Usage: $SCRIPTNAME {init|deinit|start|stop|show}"
-    exit 3
-    ;;
+  init) init ;;
+  deinit) deinit ;;
+  start) start ;;
+  stop) stop ;;
+  show) show ;;
+  *) SCRIPTNAME="${0##*/}"
+     echo "Usage: $SCRIPTNAME {init|deinit|start|stop|show}"
+     exit 3
+     ;;
 esac
 
 exit 0
